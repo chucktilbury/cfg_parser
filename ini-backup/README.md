4176d819d07980095ce9aa6a09a88e1c67d893db
Chuck Tilbury <chuck@mellowthings.com>
2022-08-14 18:50:11 -0500

    get rid of old backup directory

--- ini-backup/README.md
+++
@@ -1,2 +0,0 @@
-# ini_parser
-This is a simple INI file parser that is intended to be used for runtime configuration in a compiler.
--- ini-backup/cfg_parser.h
+++
@@ -1,8 +0,0 @@
-#ifndef CFG_PARSER_H
-#define CFG_PARSER_H
-
-// void loadCfg(const char* fname);
-// const char* getCfgEntry(const char* entry);
-// void addCfgEntry(const char* entry);
-
-#endif
